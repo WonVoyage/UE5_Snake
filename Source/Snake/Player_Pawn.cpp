22763b26902a587871954b03f9a132b859f5a587
WonVoyage <WonVoyage@yandex.ru>
2023-07-14 17:37:55 +0300

    Add camera
    * Added a camera component and rotated it to -90 degrees.

--- Source/Snake/Player_Pawn.cpp
+++ Source/Snake/Player_Pawn.cpp
@@ -1,9 +1,13 @@
 #include "Player_Pawn.h"
+#include "Engine/Classes/Camera/CameraComponent.h"
 
 //-------------------------------------------------------------------------------------------------------------
 APlayer_Pawn::APlayer_Pawn()
 {
 	PrimaryActorTick.bCanEverTick = true;
+	Player_Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Player_Camera"));
+	
+	Player_Camera->SetWorldRotation(FRotator(-90, 0, 0));
 }
 //-------------------------------------------------------------------------------------------------------------
 void APlayer_Pawn::BeginPlay()
--- Source/Snake/Player_Pawn.h
+++ Source/Snake/Player_Pawn.h
@@ -4,20 +4,22 @@
 #include "GameFramework/Pawn.h"
 #include "Player_Pawn.generated.h"
 
+class UCameraComponent;
+
 //-------------------------------------------------------------------------------------------------------------
 UCLASS() class SNAKE_API APlayer_Pawn : public APawn
 {
 	GENERATED_BODY()
 
 public:
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(BlueprintReadWrite) UCameraComponent *Player_Camera;
+
 	APlayer_Pawn();
 
 protected:
 	virtual void BeginPlay() override;
-
-public:	
-	virtual void Tick(float DeltaTime) override;
-
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 };
 //-------------------------------------------------------------------------------------------------------------
